da87dc80282050355fc53e9ec05b3f93c6cee39b
William Asaba <asabawilliamdk@yahoo.com>
2025-10-22 17:22:36 +0300

    refactor: update facade to use ModuleManager
    - Changed facade binding to point to ModuleManager class
    - Updated return type and indentation

--- src/Facades/Modular.php
+++ src/Facades/Modular.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Facade;
  */
 class Modular extends Facade
 {
-    protected static function getFacadeAccessor(): string
-    {
-        return \Williamug\Modular\Modular::class;
-    }
+  protected static function getFacadeAccessor(): string
+  {
+    return \Williamug\Modular\ModuleManager::class;
+  }
 }
