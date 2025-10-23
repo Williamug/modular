44318cdc9a22e9d75a159ab47acf33cdb150f0a0
William Asaba <asabawilliamdk@yahoo.com>
2025-10-23 22:02:59 +0300

    feat: make modules path configurable via env
    Updated the modules_path configuration to be configurable via MODULAR_PATH environment variable, with 'Modules' as the default directory. This provides more flexibility for different project structures.

--- config/modular.php
+++ config/modular.php
@@ -5,7 +5,7 @@ return [
   /**
    * Path to modules
    */
-  'modules_path' => base_path('modules'),
+  'modules_path' => env('MODULAR_PATH', base_path('Modules')),
 
   /**
    * Whether to auto-scan modules on boot (set false for manual control)
